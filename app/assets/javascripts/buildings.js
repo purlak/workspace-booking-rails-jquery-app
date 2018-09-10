ba2466c6429753357c0be7d300c8fbe571e2a8dc
github username <purnima.nyc@gmail.com>
2018-09-10 05:36:29 -0400

    clean up code

--- app/assets/javascripts/buildings.js
+++ app/assets/javascripts/buildings.js
@@ -15,39 +15,27 @@ var getBuildingIds = () => {
 }
 
 var bindBuildingClickHandlers = () => {
-    //console.log('bind click handlers in building.js')
      $('.next').on('click', function(e) {
         e.preventDefault()
-         //console.log(ids)
         let nextId=+($(this).attr("id")) + 1
          
          if (!ids.includes(nextId)){ 
             nextId = ids[0]
              
          }
-        //console.log('making request')
+        
          fetch(`/buildings/${nextId}.json`)
             
          .then((res) => res.json())
          .then(building => {
             $('.building-show').html(' ')
-             //debugger
-             //building.forEach((b) => {
                 let newBuilding = new Building(building)
                 let buildingHtml = newBuilding.formatIndex()
                
                 $('.building-show').append(buildingHtml)
                 $('button').attr("id", nextId);
-            //    })
             })
     })
-    
-//    $('.next-building').on('click', function(e){
-//        let nextId=+($(this).attr("id")) + 1
-//        fetch (`/buildings/${nextId}.json`)
-//        
-//        
-//    })
 }
 
 function Building(building) {
@@ -62,8 +50,6 @@ Building.prototype.formatIndex = function() {
         this.workspaces.forEach((w) => {
         workspacesHtml += `<li> ${w.workspace_type} - <a href="/workspaces/${w.id}/bookings/new">Available</a></li>`
         })
-    //debugger
-    
     let buildingHtml = `  
         <h3>Location: ${this.location_name}</h3>
         <h4>Address: ${this.address}</h4>
@@ -74,10 +60,3 @@ Building.prototype.formatIndex = function() {
     return buildingHtml
     
 }
-
-//Building.prototype.formatShow=function(){
-//    let buildingHtml = ` 
-//    <h4>Booking Date: ${this.booking_date}</h4>
-//`
-//    return bookingHtml
-//}
